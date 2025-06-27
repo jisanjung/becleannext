eedf2601662a6cab604bbf72ab78dea492fb1cb4
Jason Jung <jjjung13@gmail.com>
2025-06-27 11:18:28 -0400

    install third party next and add GoogleTagManager

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import { GTM_ID } from "@/constants";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +22,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer/>
       </body>
+      <GoogleTagManager gtmId={GTM_ID}/>
     </html>
   );
 }
