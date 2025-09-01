4d13c1eda5b25dc47bc4ec439eff884e45ea7092
Jisan Jung <jjjung13@gmail.com>
2025-09-01 14:31:03 -0500

    start on referral page

---
+++ src/app/referral/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+  return (
+    <section>
+        <h1>Keep Your Car's Shine for 8+ Years - VIP Ceramic Coating</h1>
+        <p>Free pickup, same-day delivery, and premium ceramic protection that lasts.</p>
+        <img
+            src='/referral/before_after.png'
+        />
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/app/referral/page.js
@@ -0,0 +1,16 @@
+import Referral from "./Referral";
+
+export const metadata = {
+    title: "test referral page | beclean",
+    description: "referral page description",
+};
+
+const ReferralWrapper = () => {
+  return (
+    <main>
+        <Referral/>
+    </main>
+  )
+}
+
+export default ReferralWrapper
