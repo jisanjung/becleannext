6e549ea7f133e0a1e23963b39250abc06c97ff73
Jisan Jung <jjjung13@gmail.com>
2025-09-01 19:28:19 -0500

    hero section for referral page

--- src/app/referral/Hero.jsx
+++ src/app/referral/Hero.jsx
@@ -1,12 +1,48 @@
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <section>
-        <h1>Keep Your Car's Shine for 8+ Years - VIP Ceramic Coating</h1>
-        <p>Free pickup, same-day delivery, and premium ceramic protection that lasts.</p>
+    <section className="wrapper px-4 py-8">
+        <h1 className="text-2xl font-bold text-center mb-2">Keep Your Car's Shine for 8+ Years - VIP Ceramic Coating</h1>
+        <p className="text-gray-500 text-center mb-4">Free pickup, same-day delivery, and premium ceramic protection that lasts.</p>
         <img
             src='/referral/before_after.png'
+            className="rounded-2xl"
         />
+        <ul className="my-6">
+            <li className="flex items-center mb-2">
+                <FaRegCheckCircle 
+                    className="flex-shrink-0 mr-2 text-xl"
+                    style={{ color: '#43BDD5' }}
+                />
+                <p>Pickup and Same Day Delivery (Harleysville + 50 Miles)</p>
+            </li>
+            <li className="flex items-center mb-2">
+                <FaRegCheckCircle 
+                    className="flex-shrink-0 mr-2 text-xl"
+                    style={{ color: '#43BDD5' }}
+                />
+                <p>Bonus Glass Ceramic Coating</p>
+            </li>
+            <li className="flex items-center mb-2">
+                <FaRegCheckCircle 
+                    className="flex-shrink-0 mr-2 text-xl"
+                    style={{ color: '#43BDD5' }}
+                />
+                <p>30 - Day Follow-Up (Spot Fix)</p>
+            </li>
+            <li className="flex items-center mb-2">
+                <FaRegCheckCircle 
+                    className="flex-shrink-0 mr-2 text-xl"
+                    style={{ color: '#43BDD5' }}
+                />
+                <p>5 Spots / Week</p>
+            </li>
+        </ul>
+        <small className="text-center block text-orange-500 mb-1">Only 3 spots remaining</small>
+        <a href="/book" className="button block text-center">
+            Reserve My Spot
+        </a>
     </section>
   )
 }
