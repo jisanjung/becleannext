f4e6b166dca210c86ab93f8e29bdc346fba139be
Jisan Jung <jjjung13@gmail.com>
2025-09-23 22:55:31 -0500

    referral - title + description

--- src/app/referral/page.js
+++ src/app/referral/page.js
@@ -1,8 +1,8 @@
 import Referral from "./Referral";
 
 export const metadata = {
-    title: "test referral page | beclean",
-    description: "referral page description",
+    title: "Affiliate 1 VIP Ceramic Coating | beclean",
+    description: "Exclusive ceramic coating services offered only at beclean. 8 year protection, free pickup & delivery, signature aftercare kit, plus 30-day guarantee.",
 };
 
 const ReferralWrapper = () => {
