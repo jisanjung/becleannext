5a09b2f6d90569e89ef335942a9f2ad310f2cb5d
Jisan Jung <jjjung13@gmail.com>
2025-10-17 21:50:24 -0500

    fix images attempt 1

--- src/app/referral/Testimonials.jsx
+++ src/app/referral/Testimonials.jsx
@@ -5,10 +5,10 @@ const Testimonials = () => {
     <section className='wrapper px-4 py-8 bg-gray-200'>
         <h1 className="text-2xl font-bold text-center mb-4">What Our Clients Say</h1>
         <div className='flex flex-col '>
-            <img src='/referral/review1.PNG' className='mb-4 rounded-2xl'/>
-            <img src='/referral/review2.PNG' className='mb-4 rounded-2xl'/>
-            <img src='/referral/review3.PNG' className='mb-4 rounded-2xl'/>
-            <img src='/referral/review4.PNG' className='mb-4 rounded-2xl'/>
+            <img src='/referral/review1.png' className='mb-4 rounded-2xl'/>
+            <img src='/referral/review2.png' className='mb-4 rounded-2xl'/>
+            <img src='/referral/review3.png' className='mb-4 rounded-2xl'/>
+            <img src='/referral/review4.png' className='mb-4 rounded-2xl'/>
             <img src='/referral/review5.PNG' className='mb-4 rounded-2xl'/>
             <img src='/referral/review6.PNG' className='mb-4 rounded-2xl'/>
             <img src='/referral/review7.PNG' className='mb-4 rounded-2xl'/>
